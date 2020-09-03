1ec6edee6bae2dd1640ebc264493b1cce9c19ee1
williambayliss <wmbayliss@gmail.com>
2020-09-03 19:01:34 -0400

    Can delete node with no, or two children, 1 child still buggy

--- binarysearchtree.rb
+++ binarysearchtree.rb
@@ -1,11 +1,12 @@
 module BinarySearchTree
   class Node
-    attr_accessor :data, :left, :right
+    attr_accessor :data, :left, :right, :parent
 
     def initialize data
       @data = data
       @left = nil
       @right = nil
+      @parent = nil
     end
 
     include Comparable
@@ -17,16 +18,10 @@ module BinarySearchTree
   class Tree
     attr_reader :root
     def initialize
-      test_array = [1, 3, 8, 7, 10, 15, 20, 25, 30]
+      test_array = [1, 2, 3, 4, 8, 7, 17, 20, 24, 27, 30]
 
       @root = build_tree(test_array)
-      insert(12)
-      insert(4)
-      insert(5)
-      insert(45)
-      insert(126)
-      insert(23)
-      delete(8)
+      delete(20)
       pretty_print()
     end
   
@@ -58,29 +53,14 @@ module BinarySearchTree
       #    Recursively build the tree from the middle of the array out, using recursion to return the nodes you make on each cycle into the tree
       root_node = Node.new(data)
       root_node.left = build_tree(left_array)
+      root_node.left.parent = root_node
       root_node.right = build_tree(right_array)
+      root_node.right.parent = root_node
   
       return root_node
     end
   
-    def find value, node=@root
-      #    Base case
-      #    Check for nil node, or !nil node but nil data as a failsafe
-      if node.nil? || node.data.nil?
-        return 1
-      #    if value found return node
-      elsif node.data == value
-        return node
-      end
-      
-      #    If value not equal to current node, recursively iterate through the tree until it is found and return it
-      if node.data > value
-        return find(value, node.left)
-      elsif node.data < value
-        return find(value, node.right)
-      end
-    end
-  
+    #    Given a value, will insert that value into the binary tree as a new node
     def insert  node_data, current_node=@root
       #    New node to be inserted
       new_node = Node.new(node_data)
@@ -116,55 +96,69 @@ module BinarySearchTree
       end
     end
   
-    def delete  node_data, root_node=@root, &block
-      return root_node if root_node.data.nil?
+    def delete  node_data, root_node=find(node_data)
+      #    Special case, if node is tree root
+      if root_node == @root
+        @root = null
+        return
+      end
 
-      case node_data <=> root_node.data
-        #    If less than, search left half
-        when -1
-          delete(node_data, root_node.left)
-        #    If greater than, search right half
-        when 1
-          delete(node_data, root_node.right)
-        #    If <=> returns 0, node found
-        when 0
-          #    No children
-          if root_node.left.nil? && root_node.right.nil?
-            daddy = find_parent(root_node)
-            if daddy.left.data == root_node.data
-              daddy.left = nil
-            elsif daddy.right.data == root_node.data
-              daddy.right = nil
-            end
+      if root_node.nil?
+        return 1
+      end
+      #    If node has two children
+      if root_node.right && root_node.left
+        min_order_node = min_order(root_node.right)
+        temp = min_order_node.data
+        delete(temp)
+        root_node.data = temp
+      
+        #    Node has no subtrees, orphan node
+      elsif root_node.right.nil? && root_node.left.nil?
+        if root_node.data < root_node.parent.data
+          root_node.parent.left = nil 
+        elsif root_node.data > root_node.parent.data
+          root_node.parent.right = nil
+        end
+        return root_node.data
+      #    Node has 1 child
+      else
+          #    If left child exists
+          unless root_node.left.nil?
+          
+            temp = root_node.left.data
+            delete(temp)
+            root_node.data = temp
+            return root_node.data
           end
-
-          #    One child
-
-          #    Two children
-
+          unless root_node.right.nil?
+            temp = root_node.right.data
+            delete(temp)
+            root_node.data = temp
+            return root_node.data
+          end
+        end
       end
-
-      root_node
-    end
-
-    def find_parent node, root_node=@root
-      if root_node.left.data == node.data
-        return root_node
-      elsif root_node.right.data == node.data
-        return root_node
-      end
-
-      case node.data <=> root_node.data
-      when -1
-        find_parent(node, root_node.left)
-      when 1
-        find_parent(node, root_node.right)
-      end
-    end
+        
     
+    def find value, root_node=@root
+      #    If value not equal to current node, recursively iterate through the tree until it is found and return it
+      case value <=> root_node.data
+        when -1
+          find(value, root_node.left)
+        when 1
+          find(value, root_node.right)
+        when 0
+          return root_node
+        else
+          return
+      end
+    end
 
-    def leftest_node node
-      current = node
+    #    Find the leftmost node in any given tree, used in deletion function
+    #    For when a node has two children
+    def min_order root_node=@root
+      current = root_node
 
       until current.left.nil?
         current = current.left
@@ -172,6 +166,8 @@ module BinarySearchTree
 
       return current
     end
+
+
     #    Prints out visualization of the tree
     def pretty_print(node = @root, prefix = '', is_left = true)
       unless node.right.nil?
